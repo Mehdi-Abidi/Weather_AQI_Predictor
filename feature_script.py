1385e88dad65f761df403bbf0d30a72e50e5d827
furzan ahmed <156594010+furzan@users.noreply.github.com>
2025-01-26 14:48:59 +0500

    Update feature_script.py

--- feature_script.py
+++ feature_script.py
@@ -148,5 +148,5 @@ def fetch_and_process_data(api, s, days):
 
 if __name__ == "__main__":
     start_date = datetime.datetime.today()
-    days = 5
+    days = 365
     fetch_and_process_data(API, start_date, days)
